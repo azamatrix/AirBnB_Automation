3737a35a9829d0d668bd42f3515f22e216b650fa
RyskulievA <azamat.ryskuliev@gmail.com>
2024-12-25 10:19:32 +0600

    BugFix1

---
+++ src/main/java/bugfix/BugFix1.java
@@ -0,0 +1,7 @@
+package bugfix;
+
+public class BugFix1 {
+    public static void main(String[] args) {
+        System.out.println("BugFix1");
+    }
+}
---
+++ src/main/java/bugfix/BugFix2.java
@@ -0,0 +1,7 @@
+package bugfix;
+
+public class BugFix2 {
+    public static void main(String[] args) {
+        System.out.println("BugFix2");
+    }
+}
